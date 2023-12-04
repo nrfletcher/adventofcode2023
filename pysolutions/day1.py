be6e08b9a5bf1080db151fb1db87eacc7b8cd1b3
riley <nrileyfletcher@gmail.com>
2023-12-03 22:16:37 -0500

    day1py

--- pysolutions/day1.py
+++ pysolutions/day1.py
@@ -1,1 +1,45 @@
-print('day 1')
+file_path = "input.txt"
+total = 0
+cnt = 1
+nums = {'one': 3, 'two': 3, 'three': 5, 'four': 4, 'five': 4, 'six': 3, 'seven': 5, 'eight': 5, 'nine': 4}
+convert = {'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}
+
+try:
+    with open(file_path, "r") as file:
+        
+        for line in file:
+            cnt += 1
+            char1 = ''
+            char2 = ''
+            current = 0
+            seen = 0
+            for c in range(len(line)):
+                if line[c] in '123456789':
+                    if seen == 0:
+                        char1 = line[c]
+                        seen += 1
+                    else:
+                        char2 = line[c]
+                elif line[c] in 'otfsen':
+                        for num in nums:
+                            if (nums[num] + c) < len(line):
+                                if line[c:c+nums[num]] == num:
+                                    if seen == 0:
+                                        char1 = convert[num]
+                                        seen += 1
+                                    else:
+                                        char2 = convert[num]
+            if char2 == '':
+                char2 = char1
+            val = char1 + char2
+            total += int(val)
+
+        print(total)
+                
+            
+except FileNotFoundError:
+    print("no such file")
+except IOError:
+    print("error reading")
+
+    
--- solutions/problem1.c
+++ solutions/problem1.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-	// using C for the first problems, will switch to C++
-	return 0;
+int function(char *str) {
+        return 0;
+}
+
+int main(int argc, char *argv[]) {
+        FILE *file;
+		file = fopen("input.txt", "r");
+        int total = 0;
+
+        for(int i = 1; i < argc; i++) {
+                char *line = argv[i];
+                char num[3];
+                char last;
+                char first;
+                int currentTotal = 0;
+                int totalInts = 0;
+
+                for(int j = 0; j < strlen(argv[i]); j++) {
+                        if(argv[i][j] >= '0' && argv[i][j] <= '9') {  
+                                if(currentTotal == 0) {
+                                        first = argv[i][j];
+                                }
+                                last = argv[i][j];
+
+                                currentTotal++;
+                        }
+                }
+                num[0] = first;
+                num[1] = last;
+                num[2] = '\0';
+
+                total += atoi(num);
+                printf("%s \n", num);
+        }
+
+        printf("%d \n", total);
+
+        return 0;
 }
