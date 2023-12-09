fef36b591c241bacd9235dc384c6d9e49a80cf71
riley <nrileyfletcher@gmail.com>
2023-12-08 22:03:20 -0500

    day3

--- pysolutions/day3.py
+++ pysolutions/day3.py
@@ -4,27 +4,31 @@ from task import get_input
 lines = get_input(sys.argv[1])
 
 def parttwo():
-    partnumsums = 0
+    # If we see a gear, check for adjacent numbers (check all 8 surrounding spots, and count instances)
+    # If total adjacent is == 2, get those two numbers, multiply them, and add to our total
+    gearratiosums = 0
 
     for row in range(len(lines)):
-        curr_num = ''
-        begin = 0
         for j, char in enumerate(lines[row]):
-            
-            if char in '1234567890':
-                curr_num += char
-            if char in '123456789' and j > 0 and len(curr_num ) == 1:
-                begin = j
 
-            if len(curr_num) > 0 and char not in '1234567890' or (j == len(lines[0]) - 1 and len(curr_num) > 0):
+            if char == '*':
+                count = 0
                 for rowi in range(row - 1, row + 2):
-                    for colj in range(begin - 1, j + 1):
-                        if rowi >= 0 and rowi < len(lines) and j >= 0 and j < len(lines[0]):
-                            if lines[rowi][colj] not in '1234567890.':
-                                partnumsums += int(curr_num)
-                curr_num = ''
+                    seen = False
+                    for colj in range(j - 1, j + 2):
+                        if rowi >= 0 and rowi < len(lines) and j >= 0 and j < len(lines[0]) and lines[rowi][colj] in '1234567890':
+                            seen = True
+                        else:
+                            if seen:
+                                count += 1
+                                seen = False
+                    if seen:
+                        count += 1
+                if count > 0:
+                    print(f'{rowi} {colj} {count}')
+                            
 
-    print(partnumsums)
+    # print(partnumsums)
 
 
 def partone():
