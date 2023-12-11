baf324ee2b7137cba41f6142ed28b2d6cb487809
riley <nrileyfletcher@gmail.com>
2023-12-10 22:22:51 -0500

    day4

--- pysolutions/day4.py
+++ pysolutions/day4.py
@@ -4,10 +4,25 @@ from task import get_input
 cards = get_input(sys.argv[1])
 
 def parttwo():
-    pile_total = 0
+    cardcounts = {}
     for card in cards:
-        card_total = 0
+        cardnum = ''
+        for ch in card:
+            if ch == ':':
+                break
+            if ch.isdigit():
+                cardnum += ch
+        cardcounts[cardnum] = 1
+    for card in cards:
         start = 0
+        wincount = 0
+        cardnum = ''
+        for ch in card:
+            if ch == ':':
+                break
+            if ch.isdigit():
+                cardnum += ch
+
         for ch in range(8, len(card)):
             if card[ch] == '|':
                 start = ch + 1
@@ -16,14 +31,13 @@ def parttwo():
         numbers = [value for value in card[start:].split(' ') if value]
         for number in numbers:
             if number in winners:
-                if card_total == 0:
-                    card_total = 1
-                else:
-                    card_total *= 2
-        pile_total += card_total
-
-    print(pile_total)
-
+                wincount += 1
+        for i in range(int(cardnum), int(cardnum) + wincount):
+            cardcounts[str(i + 1)] += 1 * cardcounts[cardnum]
+    total = 0
+    for card in cardcounts:
+        total += cardcounts[card]
+    print(total)
 
 def partone():
     pile_total = 0
@@ -45,3 +59,5 @@ def partone():
         pile_total += card_total
 
     print(pile_total)
+
+parttwo()
